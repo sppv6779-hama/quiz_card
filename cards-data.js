ca70dbe352a20384ac8954b229e2fa13c2804f04
sppv6779-hama <sppv6779@gmail.com>
2026-05-23 23:05:55 +0900

    Externalize card data and add gacha video sequence

---
+++ cards-data.js
@@ -0,0 +1,69 @@
+const SCIENCE_CARD_PACKS = [
+  {
+    id: "pack_continental_champions",
+    name: "大陸の覇者",
+    description: "各大陸を代表する生物カードパック。",
+    cards: [
+      {
+        id: "cc001",
+        name: "コモドドラゴン",
+        rarity: "N",
+        imageUrl: "https://www.image2url.com/r2/default/images/1779542479342-9d591258-b515-45dd-922d-9ff6a1f8d769.png"
+      },
+      {
+        id: "cc002",
+        name: "オオカミ",
+        rarity: "N",
+        imageUrl: "https://www.image2url.com/r2/default/images/1779542688702-d19b37af-ac67-486e-a1c5-249033d071c7.png"
+      },
+      {
+        id: "cc003",
+        name: "サイ",
+        rarity: "N",
+        imageUrl: "https://www.image2url.com/r2/default/images/1779542754525-0b277f23-7081-4269-8751-5c193ea5ec99.png"
+      },
+      {
+        id: "cc004",
+        name: "バッファロー",
+        rarity: "R",
+        imageUrl: "https://www.image2url.com/r2/default/images/1779542792522-4164c84a-8752-457c-9f5f-9145c4f32379.png"
+      },
+      {
+        id: "cc005",
+        name: "ゴリラ",
+        rarity: "R",
+        imageUrl: "https://www.image2url.com/r2/default/images/1779542841832-18ceb244-8d43-4aa6-958e-25e0dc79ae17.png"
+      },
+      {
+        id: "cc006",
+        name: "チーター",
+        rarity: "R",
+        imageUrl: "https://cdn.phototourl.com/free/2026-05-23-ba41b588-8820-4429-bf3d-78f3a5f397fd.png"
+      },
+      {
+        id: "cc007",
+        name: "アフリカゾウ",
+        rarity: "SR",
+        imageUrl: "https://cdn.phototourl.com/free/2026-05-23-56a804ce-f7ff-4167-8b41-389427c92f8a.png"
+      },
+      {
+        id: "cc008",
+        name: "クロコダイル",
+        rarity: "SR",
+        imageUrl: "https://cdn.phototourl.com/free/2026-05-23-1987635f-b8f1-44ff-801e-e29320bd5cd4.png"
+      },
+      {
+        id: "cc009",
+        name: "ホッキョクグマ",
+        rarity: "SSR",
+        imageUrl: "https://cdn.phototourl.com/free/2026-05-23-5c20c63e-a64a-4910-a27d-4261b1fc0c24.png"
+      },
+      {
+        id: "cc010",
+        name: "ライオン",
+        rarity: "SSR",
+        imageUrl: "https://cdn.phototourl.com/free/2026-05-23-dac4f2b6-8cdf-40af-b3cc-04aed2df515c.png"
+      }
+    ]
+  }
+];
